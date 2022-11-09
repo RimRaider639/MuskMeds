8db8708d47e83404b62b2559c2cf0ca343eea1bb
UMARJAAN <umerbinfarooq123@gmail.com>
2022-11-10 00:58:29 +0530

    signup-login

---
+++ scripts/login.js
@@ -0,0 +1,40 @@
+let details;
+let btn=document.getElementById('submit');
+btn.onclick=async ()=>{
+    let email=document.getElementById('email').value;
+     let password=document.getElementById('password').value;
+    let response=await fetch(`https://infinite-river-74709.herokuapp.com/userData?email=${email}`);
+    let data=await response.json();
+      checkdata(data,email)
+      console.log(details)
+      if(details==null){
+        alert('Email does not exist');
+      }else{
+        if(details.password==password){
+            alert('Login successfull');
+        }else{
+            alert('Wrong password')
+        }
+      }
+      details=null;
+}
+
+const checkdata=(data,enteredemail)=>{
+    data.forEach((el)=>{
+       if(el.email==enteredemail){
+         details=el;
+         return;
+       }
+    })
+}
+/////DELETE FUNCION//////
+//  let delte =async()=>{
+//     let  response=await fetch('https://infinite-river-74709.herokuapp.com/userData/',{
+//     method:'DELETE',
+//     headers:{
+//         'Content-type':'application/json'
+//     }
+// });
+// let data=await response.json();
+// }
+// delte();
---
+++ scripts/signup.js
@@ -0,0 +1,71 @@
+class User{
+        constructor(){
+           
+        }
+        validateUS(name){
+          return name.length<=3?false:true
+        };
+        validatePassword(password){
+          let x=password.includes('!')||password.includes('@')||password.includes('#')||password.includes('&')||password.includes('$');
+          let p=password.toString();
+          if(p==""){
+            alert('password is empty')
+          }
+          let y=p.length>=8;
+          let z=p[0]==p[0].toUpperCase();
+          if(x&&y&&z){
+            return true;
+          }else{
+            return false;
+          }
+        };
+        async signUp(n,e,p,m){
+          let isValidated=this.validateUS(n)&&this.validatePassword(p);
+          if(isValidated){
+            this.name=n;
+            this.email=e;
+            this.password=p;
+            this.mobile=m;
+            let res=await fetch('https://infinite-river-74709.herokuapp.com/userData');
+            let userdata=await res.json();
+            let result=false;
+            userdata.forEach((el)=>{
+          if(el.email==e){
+             result=true;
+             return
+           }
+        })
+            if(result==false){
+              let response=await fetch('https://infinite-river-74709.herokuapp.com/userData',{
+              method:'POST',
+              body:JSON.stringify(this),
+              headers:{
+                'Content-type':'application/json'
+              }
+            })
+            let data=await response.json();
+            console.log(data);
+            window.location.href="login.html";
+            }else{
+              alert('Email already exists')
+            }
+         }else{
+          alert('Please enter valid credentials')
+        }
+      }  
+    } 
+    let user=new User();
+   
+    const Register=()=>{
+
+        const name=document.getElementById('name').value;
+        const email=document.getElementById('email').value;
+        const password=document.getElementById('password').value;
+        const mobile=document.getElementById('moile').value;
+        user.signUp(name,email,password,mobile);
+        console.log(user);
+    }
+    let signupbtn=document.getElementById("confirm");
+    signupbtn.addEventListener("click",function(){
+      Register();
+    });
