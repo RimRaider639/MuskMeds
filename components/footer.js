a18cdb7837d5a7d9e6c6630b04febbe0da7928c3
Kanwarpal-Singh <111496199+Kanwarpal-Singh@users.noreply.github.com>
2022-11-10 22:45:39 +0530

    footer

---
+++ components/footer.js
@@ -0,0 +1,92 @@
+const footer = () =>{
+    return `<div id="footer-top">
+    <div id="know-us"><h3>Know Us</h3> 
+        <a href=""><p>About Us</p></a>
+        <a href=""><p>Contact Us</p></a>
+        <a href=""><p>Press Coverage</p></a>
+        <a href=""><p>Careers</p></a>
+        <a href=""><p>Business Partnership</p></a>
+        <a href=""><p>Become a Health Partner</p></a>
+        <a href=""><p>Corporate</p></a>
+    </div>
+    <div id="policies"> <h3>Our Policies</h3>
+        <a href=""><p>Privacy Policy</p></a>
+        <a href=""><p>Terms & Conditions</p></a>
+        <a href=""><p>Editorial Policy</p></a>
+        <a href=""><p>Return Policy</p></a>
+        <a href=""><p>IP Policy</p></a>
+        <a href=""><p>Grievance Redressal Policy</p></a>
+    </div>
+    <div id="services"><h3>Our Services </h3>
+        <a href=""><p>Order Medicines</p></a>
+        <a href=""><p>Book Lab Tests</p></a>
+        <a href=""><p>Consult a Doctor</p></a>
+        <a href=""><p>Ayurveda Articles</p></a>
+        <a href=""><p>Hindi Articles</p></a>
+        <a href=""><p>Care Plan</p></a>
+    </div>
+    <div id="connect"><h3>Connect</h3> 
+        <div id="cnt">
+            <div id="cntptag">
+                <p>Social Links</p>
+            </div>
+            <div id="cntlinks">
+                <img src="https://onemg.gumlet.io/vqpr5zx9ofpsyafjwwin.svg" alt="">
+                <img src="https://onemg.gumlet.io/wwynoy59i3iakt8te5xl.svg" alt="">
+                <img src="https://onemg.gumlet.io/cwrpdmlzckwzvv9e1gjv.svg" alt="">
+                <img src="https://onemg.gumlet.io/tywtfwyzxb8ujnqmbuau.svg" alt="">
+                <img src="https://onemg.gumlet.io/hqfglkzkfxaxaypniixn.svg" alt="">
+                <img src="https://onemg.gumlet.io/ny7nk9wxxlcxpeh1jbgg.svg" alt="">
+            </div>
+        </div>
+        <div id="cnb">
+            <div>
+                <p>Want daily dose of health?</p>
+            </div>
+            <div id="signup2">SIGN UP</div>
+        </div>
+    </div>
+    <div id="apps"><h3>Download</h3>  
+        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKHWO6r0JldZ7xPsevBCb1CGZK6qGjxYCXRw&usqp=CAU" alt=""></div>
+    </div>
+</div>
+
+<div id="footer-mid">
+    <div id="mid-left">
+        <div id="midleft1">
+            <img src="https://onemg.gumlet.io/secure-rebrand_x6f8yq.svg" alt="reliable">
+        </div>
+        <div id="midleft2">
+            <h2>Reliable</h2>
+            <p>All Products displayed Musk Meds ae procured from verified and licensed pharmacists. All labs listed on the platform are accredited</p>
+        </div>
+    </div>
+    <div id="mid-mid">
+        <div id="midmid1">
+            <img src="https://onemg.gumlet.io/reliable-rebrand_rcpof3.svg" alt="reliable">
+        </div>
+        <div id="midmid1">
+            <h2>Secure</h2>
+            <p>Musk Meds uses Secure Sockets Layer(SSL) 128-bit encryption and is Payment Card Industry Data Security Standard(PCI DSS) compliant</p>
+        </div>
+    </div>
+    <div id="mid-right">
+        <div id="midright1">
+            <img src="https://onemg.gumlet.io/affordable-rebrand_ivgidq.svg" alt="reliable">
+        </div>
+        <div id="midright2">
+            <h2>Affordable</h2>
+            <p>find affordable medicine substitutes, save up to 50% on health products, up to 80% off on lab tests and free doctor consultations.</p>
+        </div>
+    </div>
+</div>
+
+<div id="footer-bottom">
+    <div>
+        <img src="https://static.legitscript.com/seals/729605.png" alt="">
+        <img src="https://onemg.gumlet.io/URS_ebgmxc.png" alt="">
+    </div>
+    <div><b>India's Only legitScript an ISO 27001 certified online healthcare platform</b></div>
+</div>`;
+};
+export { footer };
